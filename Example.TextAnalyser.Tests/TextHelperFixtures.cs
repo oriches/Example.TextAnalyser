40862272c4389758744eef115a7617e2a675408d
Ollie Riches <ollie.riches@btinternet.com>
2014-10-27 20:22:54 +0000

    Added WordCount implementation and tests

--- Example.TextAnalyser.Tests/TextHelperFixtures.cs
+++ Example.TextAnalyser.Tests/TextHelperFixtures.cs
@@ -1,19 +1,129 @@
 ﻿namespace Example.TextAnalyser.Tests
 {
+    using System.Collections.Generic;
+    using System.Linq;
     using NUnit.Framework;
 
     [TestFixture]
     public class TextHelperFixtures
     {
         [Test]
-        public void some_test()
+        public void empty_text_returns_no_words()
         {
-            // ARRANGE
+            // GIVEN
+            var text = string.Empty;
 
-            // ACT
+            // WHEN
+            var results = TextHelper.WordCount(text);
 
-            // ASSERT
+            // THEN
+            Assert.That(results, Is.Empty);
+        }
+
+        [Test]
+        public void whitespace_text_returns_no_words()
+        {
+            // GIVEN
+            var text = "     ";
+
+            // WHEN
+            var results = TextHelper.WordCount(text);
+
+            // THEN
+            Assert.That(results, Is.Empty);
+        }
+
+        [Test]
+        public void null_text_returns_no_words()
+        {
+            // GIVEN
+            // WHEN
+            var results = TextHelper.WordCount(null);
+
+            // THEN
+            Assert.That(results, Is.Empty);
+        }
+
+        [Test]
+        public void text_with_some_capital_letters_returns_words()
+        {
+            // GIVEN
+            var text = "text Text text this IS is THis TEXT";
+
+            var expectedResult = new Dictionary<string, int>
+                                 {
+                                     { "text", 4 },
+                                     { "this", 2 },
+                                     { "is", 2 },
+                                 };
+            // WHEN
+            var results = TextHelper.WordCount(text);
+
+            // THEN
+            Assert.That(results.SequenceEqual(expectedResult), Is.True);
+        }
+
+        [Test]
+        public void text_with_spurious_spaces_returns_words()
+        {
+            // GIVEN
+            var text = "text   with extra spaces  in  the   text";
+
+            var expectedResult = new Dictionary<string, int>
+                                 {
+                                     { "text", 2 },
+                                     { "with", 1 },
+                                     { "extra", 1 },
+                                     { "spaces", 1 },
+                                     { "in", 1 },
+                                     { "the", 1 },
+                                 };
+            // WHEN
+            var results = TextHelper.WordCount(text);
+
+            // THEN
+            Assert.That(results.SequenceEqual(expectedResult), Is.True);
         }
         
+        [Test]
+        public void text_with_no_punctuation_returns_words()
+        {
+            // GIVEN
+            var text = "text text text this is is";
+
+            var expectedResult = new Dictionary<string, int>
+                                 {
+                                     { "text", 3 },
+                                     { "this", 1 },
+                                     { "is", 2 },
+                                 };
+            // WHEN
+            var results = TextHelper.WordCount(text);
+
+            // THEN
+            Assert.That(results.SequenceEqual(expectedResult), Is.True);
+        }
+
+        [Test]
+        public void text_with_punctuation_return_words()
+        {
+            // GIVEN
+            var text = "This is a statement, and so is this.";
+
+            var expectedResult = new Dictionary<string, int>
+                                 {
+                                     { "this", 2 },
+                                     { "is", 2 },
+                                     { "a", 1 },
+                                     { "statement", 1 },
+                                     { "and", 1 },
+                                     { "so", 1 },
+                                 };
+            // WHEN
+            var results = TextHelper.WordCount(text);
+
+            // THEN
+            Assert.That(results.SequenceEqual(expectedResult), Is.True);
+        }
     }
 }
--- Example.TextAnalyser/TextHelper.cs
+++ Example.TextAnalyser/TextHelper.cs
@@ -1,12 +1,23 @@
 ﻿namespace Example.TextAnalyser
 {
     using System.Collections.Generic;
+    using System.Linq;
 
     public static class TextHelper
     {
         public static IDictionary<string, int> WordCount(string text)
         {
-            var results = new Dictionary<string, int>();
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return new Dictionary<string, int>();
+            }
+
+            var results = string.Concat(text.Where(x => !char.IsPunctuation(x)))
+                .ToLower()
+                .Split(' ')
+                .Where(x => !string.IsNullOrEmpty(x))
+                .GroupBy(x => x)
+                .ToDictionary(x => x.Key, x => x.Count());
 
             return results;
         }
